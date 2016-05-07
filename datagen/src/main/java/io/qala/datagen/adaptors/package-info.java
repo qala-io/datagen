4c7264bd65b3933b025da3ae149b5d73c7e58cb2
Stanislav Bashkyrtsev <stanislav.bashkirtsev@gmail.com>
2016-05-07 22:58:20 +0300

    Add package-info for the adaptors
    Adaptors is a borrowed code from other open source projects that
    implement complicated low-level generation of random data.

---
+++ datagen/src/main/java/io/qala/datagen/adaptors/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * These are classes that were borrowed from other open source projects so that Datagen doesn't
+ * repeat that complicated logic for low-level randomization of data. Includes code from: Commons Lang, Commons Math.
+ */
+package io.qala.datagen.adaptors;
