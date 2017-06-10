1dfc85e50188cc04290926c7cfcb895721e194dd
Stanislav Bashkyrtsev <stanislav.bashkirtsev@gmail.com>
2017-06-10 10:36:03 +0300

    Add more tests for Alphanumerics

--- junit5/src/test/java/io/qala/datagen/junit5/Junit5ParameterizedTest.java
+++ junit5/src/test/java/io/qala/datagen/junit5/Junit5ParameterizedTest.java
@@ -18,7 +18,8 @@ public class Junit5ParameterizedTest {
         }
 
         @RandomInt(name = "zero")
-        void ignoresName_if2ndParamIsNotPresent(int param) {}
+        void ignoresName_if2ndParamIsNotPresent(int param) {
+        }
 
         @RandomInt(name = "blah")
         void passesName_asSecondParam(int param, String name) {
@@ -33,43 +34,74 @@ public class Junit5ParameterizedTest {
             assertNotEquals(0, param);
             assertChangedFromLastTime(param);
         }
+
         @RandomInt(name = "zero") @RandomInt
-        void ignoresName_if2ndParamIsNotPresent(int param) {}
+        void ignoresName_if2ndParamIsNotPresent(int param) {
+        }
 
         @RandomInt(name = "blah") @RandomInt(name = "blah")
         void passesName_asSecondParam(int param, String name) {
             assertEquals("blah", name);
         }
+
+        @RandomInt(name = "name") @RandomInt
+        void nameIsEmpty_byDefault(int value, String name) {
+            assertTrue(name.equals("name") || name.isEmpty());
+            assertChangedFromLastTime(name);
+        }
     }
 
+    @Nested class AlphanumericTest {
+        @Alphanumeric void generatesAlphanumeric_from1_to100_byDefault(String value) {
+            assertTrue(value.trim().length() >= 1);
+            assertTrue(value.length() <= 100);
+        }
 
-    @Alphanumeric void generatesAlphanumeric_from1_to100_byDefault(String value) {
-        assertTrue(value.trim().length() >= 1);
-        assertTrue(value.length() <= 100);
+        @Alphanumeric(name = "name") void setsNameAsArgumentIf2ndParameterExists(String value, String name) {
+            assertEquals("name", name);
+        }
+        @Alphanumeric(name = "name") void ignoresName_if2ndParamIsAbsent(String value) {}
+
+        @Alphanumeric(length = 29) void generatesFixedLengthAlphanumeric_ifLengthIsSet(String value) {
+            assertEquals(29, value.length());
+        }
+        @Alphanumeric(min = 2, max = 3) void lengthIsTakenFrom_minMaxParams_ifLengthIsAbsent(String value) {
+            assertTrue(value.length() >= 2 && value.length() <= 3);
+        }
     }
 
-    @Alphanumeric(name = "name") void setsNameAsArgumentIf2ndParameterExists(String value, String name) {
-        assertEquals("name", name);
-    }
+    @Nested class AlphanumericsTest {
+        @Alphanumeric(length = 1)
+        @Alphanumeric(min = 2, max = 29)
+        @Alphanumeric(length = 30)
+        void canGenerateMultipleAlphanumerics(String value) {
+            assertTrue(value.length() >= 1 && value.length() <= 31);
+            assertChangedFromLastTime(value);
+        }
 
-    @Alphanumeric(length = 29) void generatesFixedLengthAlphanumeric_ifLengthIsSet(String value) {
-        assertEquals(29, value.length());
-    }
+        @Alphanumeric(length = 1, name = "min boundary")
+        @Alphanumeric(min = 2, max = 29, name = "middle value")
+        @Alphanumeric(length = 30, name = "max boundary")
+        void ignoresCaseName_ifItIsNotPassed(String value) {
+            assertTrue(value.length() >= 1 && value.length() <= 31);
+            assertChangedFromLastTime(value);
+        }
 
-    @Alphanumeric(length = 1, name = "min boundary")
-    @Alphanumeric(min = 2, max = 29, name = "middle value")
-    @Alphanumeric(length = 30, name = "max boundary")
-    void canGenerateMultipleAlphanumerics(String value, String caseName) {
-        assertTrue(value.length() >= 1 && value.length() <= 31, "Failed: " + caseName);
-        assertChangedFromLastTime(value);
-    }
+        @Alphanumeric(min = 2, max = 3, name = "name1")
+        @Alphanumeric(length = 1, name = "name2")
+        void passesName_if2ndParam_isPresent(String value, String name) {
+            assertTrue(name.startsWith("name"));
+            assertChangedFromLastTime(name);
 
-    @Alphanumeric(length = 1, name = "min boundary")
-    @Alphanumeric(min = 2, max = 29, name = "middle value")
-    @Alphanumeric(length = 30, name = "max boundary")
-    void ignoresCaseName_ifItIsNotPassed(String value) {
-        assertTrue(value.length() >= 1 && value.length() <= 31);
-        assertChangedFromLastTime(value);
+            assertTrue(value.length() >= 1 && value.length() <= 31);
+            assertChangedFromLastTime(value);
+        }
+        @Alphanumeric(min = 2, max = 3, name = "name")
+        @Alphanumeric(length = 1)
+        void nameIsEmpty_byDefault(String value, String name) {
+            assertTrue(name.equals("name") || name.isEmpty());
+            assertChangedFromLastTime(name);
+        }
     }
 
     private static void assertChangedFromLastTime(Object newValue) {
